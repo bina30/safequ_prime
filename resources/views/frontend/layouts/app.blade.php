364710761bc556a3b3b6497e45013b11a720c003
Pinark Patel <pinarkpatdj1911@gmail.com>
2022-03-14 13:39:52 +0530

    Request otp inputs hange js added in footer

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -348,6 +348,31 @@
                     }
                 }, 15);
             });
+
+            $('.otp-form').find('input').each(function () {
+                $(this).attr('maxlength', 1);
+                $(this).on('keyup touchend', function (e) {
+                    var parent = $($(this).parent());
+
+                    if (e.keyCode === 8 || e.keyCode === 37) {
+                        var prev = parent.find('input#' + $(this).data('previous'));
+
+                        if (prev.length) {
+                            $(prev).select();
+                        }
+                    } else if ($.isNumeric($(this).val())) {
+                        var next = parent.find('input#' + $(this).data('next'));
+
+                        if (next.length) {
+                            $(next).select();
+                        } else {
+                            if (parent.data('autosubmit')) {
+                                parent.submit();
+                            }
+                        }
+                    }
+                });
+            });
         });
 
         $(window).on('scroll', function() {
