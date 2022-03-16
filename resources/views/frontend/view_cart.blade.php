9aed1c6ea38205a7b6cce2159202776b2a970f7a
rocko491 <srushil64@gmail.com>
2022-03-16 13:54:58 +0530

    - Done cart update, delete and order confirmed page

--- resources/views/frontend/view_cart.blade.php
+++ resources/views/frontend/view_cart.blade.php
@@ -1,5 +1,9 @@
 @extends('frontend.layouts.app', ['header_show' => true, 'header2' => true, 'footer' => false])
 
+@php
+    $total = 0;
+@endphp
+
 @section('content')
     <main class="main-tag mt-0">
 
@@ -9,7 +13,8 @@
             </div>
         </div>
 
-        <div class="content pb-5">
+
+        <div class="content pb-5" id="cart_summary">
             <div class="container py-4">
                 <div class="row justify-content-center">
                     <div class="col-lg-6 col-md-7 px-1">
@@ -29,9 +34,6 @@
                         <br>
                         <!-- Item Card -->
 
-                        @php
-                            $total = 0;
-                        @endphp
                         @foreach ($carts as $key => $cartItem)
                             @php
                                 $product = \App\Models\Product::find($cartItem['product_id']);
@@ -43,32 +45,36 @@
                                 <div class="img-name w-100">
                                     <div class="p-0 mxw-85px">
                                         <div class="item-img text-center">
-                                            <img src="{{ uploaded_asset($product->photos) }}" alt="Item image" />
+                                            <img src="{{ uploaded_asset($product->photos) }}" onerror="this.onerror=null;this.src='{{ static_asset('assets/img/avatar-place.png') }}';" alt="{{ $product->name }}" />
                                         </div>
                                     </div>
                                     <div class="pl-3 w-100">
                                         <h6 class="fw700">{{ $product->name }}</h6>
                                         <div class="pt-2 d-flex">
                                             <p class="body-txt mb-0">
-                                            <span class="act-price fw700">
-                                                {!! single_price($sub_total) !!}
-                                            </span>
+                                        <span class="act-price fw700">
+                                            {!! single_price($sub_total) !!}
+                                        </span>
                                                 <i class="body-txt fsize12">&nbsp; <br class="sm" />
                                                     ({!! single_price($product->unit_price) !!} / $product->unit)
                                                 </i>
                                             </p>
                                             <div class="action">
-                                                <div class="item-count">
+                                                <div class="item-count flex-acenter-jbtw">
                                                     <button class="btn"
                                                             onclick="this.parentNode.querySelector('input[type=number]').stepDown();"
-                                                            type="button"><i class="fa fa-minus"></i></button>
-                                                    <input class="quantity" min="1" name="quantity" value="{{ $cartItem['quantity'] }}"
-                                                           type="number" id="quantity" readonly />
+                                                            type="button" data-field="quantity[{{ $cartItem['id'] }}]" data-cart_id="{{ $cartItem['id'] }}">
+                                                        <i class="fa fa-minus"></i>
+                                                    </button>
+                                                    <input class="quantity" min="1" name="quantity[{{ $cartItem['id'] }}]" value="{{ $cartItem['quantity'] }}"
+                                                           type="number" id="quantity_{{ $cartItem['id'] }}" value="{{ $cartItem['quantity'] }}" min="1" max="{{ $product_stock->qty }}" readonly />
                                                     <button class="btn"
                                                             onclick="this.parentNode.querySelector('input[type=number]').stepUp();"
-                                                            type="button"><i class="fa fa-plus"></i></button>
+                                                            type="button" data-field="quantity[{{ $cartItem['id'] }}]" data-cart_id="{{ $cartItem['id'] }}">
+                                                        <i class="fa fa-plus"></i>
+                                                    </button>
                                                 </div>
-                                                <h6 class="mb-0 text-danger fw700 p-2 ml-2">X</h6>
+                                                <h6 class="mb-0 text-danger fw700 p-2 ml-2" onclick="removeFromCartView(event, {{ $cartItem['id'] }})">X</h6>
                                             </div>
                                         </div>
                                     </div>
@@ -133,9 +139,7 @@
                                     <p class="fsize15 mb-1 body-txt">Total:</p>
                                     <h5 class="fw500 mb-0"><ins class="currency-symbol">&#8377;</ins> {{ $total }}</h5>
                                 </div>
-                                 <button class="btn primary-btn btn-round py-1" onclick="submitOrder(this)" @if(count($carts) == 0) disabled @endif >Pay Now</button>
-
-<!--                                <a href="thank-you.html" class="btn primary-btn btn-round py-1 text-white">Pay Now</a>-->
+                                 <button id="btn_pay_now" class="btn primary-btn btn-round py-1" onclick="submitOrder(this)" @if(count($carts) == 0) disabled @endif >Pay Now</button>
                             </div>
 
                         </form>
@@ -143,6 +147,7 @@
                 </div>
             </div>
         </div>
+
     </main>
 @endsection
 
@@ -152,6 +157,30 @@
 @section('script')
 
     <script type="text/javascript">
+        $(document).ready(function() {
+            $('.item-count button').on('click', function() {
+                let cart_id = $(this).data('cart_id');
+                let qty = parseInt($("#quantity_"+cart_id).val());
+
+                $("#itm-cnt").text(qty);
+
+                if (qty >= 10) {
+                    $("#itm-cnt").removeClass("d2 d3");
+                    $("#itm-cnt").addClass("d2");
+                }
+
+                if (qty >= 100) {
+                    $("#itm-cnt").removeClass("d2 d3");
+                    $("#itm-cnt").addClass("d3");
+                }
+
+                (qty < 10) ? $("#itm-cnt").removeClass("d2 d3"): "";
+
+                $('#btn_pay_now').attr('disabled', 'disabled');
+                updateQuantity(cart_id, qty);
+            })
+        })
+
         function submitOrder(el){
             $(el).prop('disabled', true);
             if($('#delivery_address').is(":checked")){
@@ -160,6 +189,31 @@
                 AIZ.plugins.notify('danger','{{ translate('You need to select the address') }}');
                 $(el).prop('disabled', false);
             }
+        }
+
+        function updateQuantity(cart_id, qty){
+            $.post('{{ route('cart.updateQuantity') }}', {
+                _token   :  AIZ.data.csrf,
+                id       :  cart_id,
+                quantity :  qty
+            }, function(data){
+                // updateNavCart(data.nav_cart_view,data.cart_count);
+                $('#cart_summary').html('');
+                $('#cart_summary').html(data.cart_view);
+                $('#btn_pay_now').removeAttr('disabled');
+            });
+        }
+
+        function removeFromCartView(e, key){
+            e.preventDefault();
+            $.post('{{ route('cart.removeFromCart') }}', {
+                _token  : AIZ.data.csrf,
+                id      :  key
+            }, function(data){
+                // updateNavCart(data.nav_cart_view,data.cart_count);
+                $('#cart_summary').html(data.cart_view);
+                AIZ.plugins.notify('success', "{{ translate('Item has been removed from cart') }}");
+            });
         }
     </script>
 
