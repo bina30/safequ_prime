79d09f6f406fa27ffbe0feb5c3e7ba9edd0a8b67
rocko491 <srushil64@gmail.com>
2022-03-22 16:20:56 +0530

    - Added script for sms template

--- db_script.sql
+++ db_script.sql
@@ -5,6 +5,6 @@ ALTER TABLE `products` ADD COLUMN `purchase_end_date` datetime NULL AFTER `purch
 
 
 /* Dt: 22-03-22 */
-INSERT INTO `sms_templates`(`id`, `identifier`, `sms_body`, `template_id`, `status`, `created_at`, `updated_at`) VALUES (NULL, NULL, NULL, NULL, 1, 'CURRENT_TIMESTAMP', 'CURRENT_TIMESTAMP');
-INSERT INTO `sms_templates`(`id`, `identifier`, `sms_body`, `template_id`, `status`, `created_at`, `updated_at`) VALUES (NULL, NULL, NULL, NULL, 1, 'CURRENT_TIMESTAMP', 'CURRENT_TIMESTAMP');
+INSERT INTO `sms_templates`(`id`, `identifier`, `sms_body`, `template_id`, `status`, `created_at`, `updated_at`) VALUES (7, 'order_confirm', 'Confirm Order', NULL, 1, '2022-03-22 15:34:58', '2022-03-22 15:35:04');
+INSERT INTO `sms_templates`(`id`, `identifier`, `sms_body`, `template_id`, `status`, `created_at`, `updated_at`) VALUES (8, 'order_cancel', 'Cancel Order', NULL, 1, '2022-03-22 15:37:44', '2022-03-22 15:37:48');
 INSERT INTO `sms_templates`(`id`, `identifier`, `sms_body`, `template_id`, `status`, `created_at`, `updated_at`) VALUES (9, 'order_shipped', 'Order Shipped', NULL, 1, '2022-03-22 15:40:14', '2022-03-22 15:40:14');
