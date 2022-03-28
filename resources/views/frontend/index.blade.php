188e6ab5ec46a9cbc3746ef613eca4e103528150
Pinark Patel <pinarkpatdj1911@gmail.com>
2022-03-28 13:14:18 +0530

    Testimonial slider auto scroll enabled

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -311,7 +311,7 @@
                 nav: true,
                 dots: false,
                 margin: 10,
-                autoplay: false,
+                autoplay: true,
                 autoplayTimeout: 4000,
                 autoplayHoverPause: false,
                 smartSpeed: 1500,
