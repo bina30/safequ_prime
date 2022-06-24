f8c52ee0cf0680c7b01b46d075d40d7bac1ea7ba
Sarthak Shah <sarthak.sd77@gmail.com>
2022-06-24 18:32:50 +0530

    - dashboard modified - customer edit bug resolved

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\Shop;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Product;
@@ -17,14 +20,14 @@ class AdminController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function admin_dashboard(Request $request)
-    {   
+    {
         CoreComponentRepository::initializeCache();
         $root_categories = Category::where('level', 0)->get();
+        $cached_data = Cache::remember('cached_data', 3600, function () use ($root_categories) {
 
-        $cached_graph_data = Cache::remember('cached_graph_data', 86400, function() use ($root_categories){
             $num_of_sale_data = null;
             $qty_data = null;
-            foreach ($root_categories as $key => $category){
+            foreach ($root_categories as $key => $category) {
                 $category_ids = \App\Utility\CategoryUtility::children_ids($category->id);
                 $category_ids[] = $category->id;
 
@@ -37,16 +40,23 @@ class AdminController extends Controller
                         $qty += $stock->qty;
                     }
                 }
-                $qty_data .= $qty.',';
-                $num_of_sale_data .= $sale.',';
+                $qty_data .= $qty . ',';
+                $num_of_sale_data .= $sale . ',';
             }
             $item['num_of_sale_data'] = $num_of_sale_data;
             $item['qty_data'] = $qty_data;
 
+            $item['total_customers'] = User::where('user_type', 'customer')->where('email_verified_at', '!=', null)->count();
+            $item['total_orders'] = Order::count();
+            $item['total_sales'] = Order::sum('grand_total');
+            $item['total_pending_payment'] = Order::where('payment_status', 'unpaid')->sum('grand_total');
+            $item['community_data'] = Shop::whereIn('user_id', verified_sellers_id())->withCount('orders', 'customers')->get();
+
+
             return $item;
         });
 
-        return view('backend.dashboard', compact('root_categories', 'cached_graph_data'));
+        return view('backend.dashboard', compact('root_categories', 'cached_data'));
     }
 
     function clearCache(Request $request)
